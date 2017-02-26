e638281cb55908e0ea831b0d290d8f81fbe5b03c
mmihira <mmihira.w@gmail.com>
2017-02-26 14:47:27 +1100

    use absolute paths for scripts

--- rust_run.sh
+++ rust_run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 base_path="/home/mihira/c/rust_ex/target/"
 
-exe_name=$(echo "$1" | sed 's/\.rs//g')
+exe_name=$(echo "$1" | sed 's/\.rs//g' | sed 's/\/.*\///g')
+echo "exe_name :  $exe_name"
+echo 'sdfsfsdf'
 if [ -e "$base_path$exe_name" ]; then
   rm "$base_path$exe_name"
 fi
